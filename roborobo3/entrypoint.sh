4d6ee7eaddd09ba220d99682de05fa5f82387350
Leo Cazenille <leo.cazenille@gmail.com>
2020-01-09 14:45:18 +0900

    up

--- roborobo3/entrypoint.sh
+++ roborobo3/entrypoint.sh
@@ -16,9 +16,9 @@ chown -R $uid /home/user
 # Launch illumination
 if [ "$executionMode" = "normal" ]; then
     if [ "$nbRuns" -eq "1" ]; then
-        exec gosu user bash -c "cd /home/user/wmee/roborobo3; mkdir /home/user/results/$configName; ln -s /home/user/results/$configName logs; ./roborobo -l config/$configName.properties; rsync -avz /home/user/results/ /home/user/finalresults/"
+        exec gosu user bash -c "cd /home/user/wmee/roborobo3; mkdir /home/user/results/$configName; ln -s /home/user/results/$configName logs; SDL_VIDEODRIVER=offscreen ./roborobo -l config/$configName.properties; rsync -avz /home/user/results/ /home/user/finalresults/"
     else
-        exec gosu user bash -c "cd /home/user/wmee/roborobo3; mkdir /home/user/results/$configName; ln -s /home/user/results/$configName logs; for i in $(seq 1 $nbRuns | tr '\n' ' '); do sleep 1; ./roborobo -l config/$configName.properties & sleep 1; done; wait; rsync -avz /home/user/results/ /home/user/finalresults/"
+        exec gosu user bash -c "cd /home/user/wmee/roborobo3; mkdir /home/user/results/$configName; ln -s /home/user/results/$configName logs; for i in $(seq 1 $nbRuns | tr '\n' ' '); do sleep 1; SDL_VIDEODRIVER=offscreen ./roborobo -l config/$configName.properties & sleep 1; done; wait; rsync -avz /home/user/results/ /home/user/finalresults/"
     fi
 fi
 
