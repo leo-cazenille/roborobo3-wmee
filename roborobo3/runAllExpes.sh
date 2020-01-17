fc86fe2ce4fc53fe524202eb276a554516892f7c
Leo Cazenille <leo.cazenille@gmail.com>
2020-01-17 23:27:33 +0900

    up

--- roborobo3/runAllExpes.sh
+++ roborobo3/runAllExpes.sh
@@ -6,9 +6,11 @@ declare -a configs
 #configs+=("wmee-3201600-baseline 50")
 
 #configs+=("wmee-3201600-WMAE4000x10x1e-4x30x10x1067200 50")
-configs+=("wmee-401600-baseline 20")
+#configs+=("wmee-401600-baseline 20")
 #configs+=("wmee-401600-WMAE4000x10x1e-4x30x10x134000 20")
 
+configs+=("wmee-401600-task1-baseline 20")
+configs+=("wmee-401600-task1-WMAE4000x10x1e-4x30x10x134000 20")
 
 printf -v allParams "%s\n" "${configs[@]}"
 #echo $allParams | xargs --max-procs=$(nproc --all) -n 2 ./runDockerExpe.sh
