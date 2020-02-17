de40c57ed5eb24366a19ba7f87211e203a6bde8c
Leo Cazenille <leo.cazenille@gmail.com>
2020-02-17 18:16:41 +0900

    up

--- roborobo3/runAllExpes.sh
+++ roborobo3/runAllExpes.sh
@@ -10,7 +10,6 @@ declare -a configs
 #configs+=("wmee-401600-WMAE4000x10x1e-4x30x10x134000 20")
 
 #configs+=("wmee-401600-task1-baseline 20")
-configs+=("wmee-2001600-task1-baseline 20")
 
 #configs+=("wmee-401600-task1-WMAE4000x10x1e-4x30x10x134000 20")
 
@@ -27,6 +26,10 @@ configs+=("wmee-2001600-task1-baseline 20")
 #configs+=("wmee-2001600-task1-WMAEMM2000x10x10x1e-4x1e-4x30x10x257600x401600 2")
 #configs+=("wmee-2001600-task1-WMAEMM2000x10x10x1e-4x1e-4x30x10x257600x401600-6x1x5x10x2x1.0 2")
 #configs+=("wmee-2001600-task1-WMAEMM2000x10x10x1e-4x1e-4x30x10x257600x401600x1751600-6x1x5x10x2x1.0 2")
+
+
+#configs+=("wmee-2001600-task1-baseline 20")
+configs+=("wmee-2001600-task1-WMAE2000x10x1e-4x30x10x1751600-4x5 2")
 #configs+=("wmee-2001600-task1-WMAEMM2000x10x10x1e-4x1e-4x30x10x257600x401600x1751600-6x1x5x10x2x1.0-4x5 2")
 
 
